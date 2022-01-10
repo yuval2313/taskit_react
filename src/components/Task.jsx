b251dbd13275a2717cd0fa033a4b13b6fce0d3e8
Yuval Levy <yuvdash@gmail.com>
2022-01-10 15:09:35 +0200

    Saving progress from laptop

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import "../styles/Task.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 function Task(props) {
   return (
     <article className="task">
       <div className="task-header">
-        <input type="text" />
+        <span className="task-title" role="textbox" contentEditable></span>
+        <i className="far fa-times-circle"></i>
       </div>
+      <hr className="separator" />
       <div className="task-content">
         <span className="task-text" role="textbox" contentEditable></span>
-      </div>
-      <div className="task-footer">
-        <span className="task-date">
-          Created At: {new Date().toDateString()}
+        <span className="task-date muted">
+          Edited: {`${new Date().getHours()}:${new Date().getMinutes()}`}
         </span>
+      </div>
+      {/* <hr className="separator" /> */}
+      <div className="task-footer">
+        <i className="far fa-bell"></i>
         <button className="btn btn-submit">Submit</button>
       </div>
     </article>
